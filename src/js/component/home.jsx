2c0bff5e414ab7902bcefb1e59bfb50022ca065b
maxischiavina <123894494+maxischiavina@users.noreply.github.com>
2023-05-05 19:17:57 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [dato, setDato] = useState("");
+	const [listaDatos, setListaDatos] = useState([]);
+	const pendiente = listaDatos.length
+	
+	function handleChange(event){
+	setDato(event.target.value)
+	};
+	
+	function handleSubmit(event){
+		event.preventDefault();
+		setListaDatos([...listaDatos, dato]);
+		setDato("");
+	}
+	
+	function borrarDato(nombre){
+	const nuevaLista = listaDatos.filter((item)=> item !== nombre);
+	setListaDatos(nuevaLista);
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bg-dark d-flex align-items center justify-content-center">
+		<div className="bg-white w-50 align-items center" >
+			<h1 className="text-center justify-content-center fs-1">Todos</h1>
+
+
+			<form onSubmit={handleSubmit}>
+  <div className="m-0"  style={{ height: 250 }}>
+    
+    <input type="text" className="form-control p-0 m-0" id="datos" aria-describedby="emailHelp" onChange={handleChange} value={dato} required />
+  
+	<ul className="p-0 m-0" style={{ listStyle: "none" }}>
+  {listaDatos.map((item, index)=> (
+    <li className="border-top border-bottom m-0 p-0" key={index}>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>{item}</span>
+        <button type="button" className="btn " onClick={() => borrarDato(item)}>x</button>
+      </div>
+    </li>
+  ))}
+</ul>
+  <p className="p-0 m-0 border">{pendiente} Items Left</p>
+  </div>
+</form>
+</div>
 		</div>
 	);
 };
